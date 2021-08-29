70a827fac0a12835d1f9bd359150cf3aa2907077
crusoebirkhead <crusoebirkhead@gmail.com>
2021-08-28 22:45:32 -0700

    Clear board button is now functional

--- script.js
+++ script.js
@@ -28,16 +28,8 @@ const gameBoard = (() => {
       }, {
         once: true
       });
-      // div.addEventListener('click', event => {
-      //         for (let i = 0; i < gameStorage.length; i++){
-      //           if (gameStorage[1] === 'X') {
-      //             console.log('you won')
-      //           }
-      //         }
-      //       })
     }
   }
-
 
   return {
     createGrid,
@@ -55,46 +47,61 @@ const player = {
 
 
 const gameFlow = (() => {
+
   //Button for starting game
-  const gameLoad = document.getElementById('start').addEventListener('click', (e) => {
+  const start = document.getElementById('start')
+  const gameLoad = start.addEventListener('click', (e) => {
     gameBoard.createGrid();
+    start.style.display = 'none';
   })
 
+  //Logic for winner
+  const announcement = document.getElementById('announcement')
   document.getElementById('grid').addEventListener('click', event => {
-    for (let i = 0; i < gameBoard.gameStorage.length; i++) {
+
       if (gameBoard.gameStorage[0] === 'X' && gameBoard.gameStorage[1] === 'X' && gameBoard.gameStorage[2] === 'X') {
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[3] === 'X' && gameBoard.gameStorage[4] === 'X' && gameBoard.gameStorage[5] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[6] === 'X' && gameBoard.gameStorage[7] === 'X' && gameBoard.gameStorage[8] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[0] === 'X' && gameBoard.gameStorage[3] === 'X' && gameBoard.gameStorage[6] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[1] === 'X' && gameBoard.gameStorage[4] === 'X' && gameBoard.gameStorage[7] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[0] === 'X' && gameBoard.gameStorage[4] === 'X' && gameBoard.gameStorage[8] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       } else if (gameBoard.gameStorage[2] === 'X' && gameBoard.gameStorage[4] === 'X' && gameBoard.gameStorage[6] === 'X'){
-        console.log('you won')
+        announcement.textContent += 'Player X wins!'
       }
       if (gameBoard.gameStorage[0] === 'O' && gameBoard.gameStorage[1] === 'O' && gameBoard.gameStorage[2] === 'O') {
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[3] === 'O' && gameBoard.gameStorage[4] === 'O' && gameBoard.gameStorage[5] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[6] === 'O' && gameBoard.gameStorage[7] === 'O' && gameBoard.gameStorage[8] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[0] === 'O' && gameBoard.gameStorage[3] === 'O' && gameBoard.gameStorage[6] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[1] === 'O' && gameBoard.gameStorage[4] === 'O' && gameBoard.gameStorage[7] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[0] === 'O' && gameBoard.gameStorage[4] === 'O' && gameBoard.gameStorage[8] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       } else if (gameBoard.gameStorage[2] === 'O' && gameBoard.gameStorage[4] === 'O' && gameBoard.gameStorage[6] === 'O'){
-        console.log('you won')
+        announcement.textContent += 'Player O wins!'
       }
-    }
   })
-
+  //Button for restarting game
+    document.getElementById('restart').addEventListener('click', event => {
+        const grid = document.getElementById('grid')
+        const square = document.querySelectorAll('.square')
+        announcement.innerHTML = '';
+        square.innerHTML = '';
+        grid.innerHTML = '';
+        start.style.display = 'inline';
+        for (let i = 0; i < gameBoard.gameStorage.length; i++){
+          gameBoard.gameStorage[i] = '';
+        }
+    })
 
   return {
     gameLoad,
